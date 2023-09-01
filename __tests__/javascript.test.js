9a12396acb399adff64fdf649c2cd87429c33a3c
Paul LeTourneau <pletourneau62@gmail.com>
2023-09-01 08:55:41 -0700

    add test 2

--- __tests__/javascript.test.js
+++ __tests__/javascript.test.js
@@ -14,6 +14,11 @@ test('it should return the ageOnEarth and name properties of the Character class
   expect(picardPerson).toEqual({name: "Captain Jean-Luc Picard", ageOnEarth: 83});
 });
 
+test('it should return the ageOnEarth and name properties of the Character class instance paulsPerson and picardPerson', () => {
+  expect(paulsPerson).toEqual({name: "Paul", ageOnEarth: 41, ageOnMercury: 170.83});
+  expect(picardPerson).toEqual({name: "Captain Jean-Luc Picard", ageOnEarth: 83, ageOnMercury: 345.83});
+});
+
 
 });
   
