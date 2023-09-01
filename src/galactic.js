6dd245fecf33ee7e63bd9dda4b65d498acb345e1
Paul LeTourneau <pletourneau62@gmail.com>
2023-09-01 13:59:01 -0700

    add more UI

--- src/galactic.js
+++ src/galactic.js
@@ -26,9 +26,9 @@ export default class Person {
   diff(years) {
     let diff = years - this.ageOnEarth;
     if (diff > 0) {
-        return diff.toFixed(2) + " Earth years until you are " + years + " on Earth, " + (diff/.24).toFixed(2) + " Mercury years until you are " + years + " on Earth, " + (diff/.62).toFixed(2) + " Venus years until you are " + years + " on Earth, " + (diff/1.88).toFixed(2) + " Mars years until you are " + years + " on Earth, and " + (diff/11.86).toFixed(2) + " Jupiter years until you are " + years + " on Earth";
+      return diff.toFixed(2) + " Earth years until you are " + years + " on Earth, " + (diff/.24).toFixed(2) + " Mercury years until you are " + years + " on Earth, " + (diff/.62).toFixed(2) + " Venus years until you are " + years + " on Earth, " + (diff/1.88).toFixed(2) + " Mars years until you are " + years + " on Earth, and " + (diff/11.86).toFixed(2) + " Jupiter years until you are " + years + " on Earth";
     } else if (diff < 0) {
-        return Math.abs(diff).toFixed(2) + " Earth years since you were " + years + " on Earth, " + (Math.abs(diff)/.24).toFixed(2) + " Mercury years since you were " + years + " on Earth, " + (Math.abs(diff)/.62).toFixed(2) + " Venus years since you were " + years + " on Earth, " + (Math.abs(diff)/1.88).toFixed(2) + " Mars years since you were " + years + " on Earth, and " + (Math.abs(diff)/11.86).toFixed(2) + " Jupiter years since you were " + years + " on Earth";
+      return Math.abs(diff).toFixed(2) + " Earth years since you were " + years + " on Earth, " + (Math.abs(diff)/.24).toFixed(2) + " Mercury years since you were " + years + " on Earth, " + (Math.abs(diff)/.62).toFixed(2) + " Venus years since you were " + years + " on Earth, " + (Math.abs(diff)/1.88).toFixed(2) + " Mars years since you were " + years + " on Earth, and " + (Math.abs(diff)/11.86).toFixed(2) + " Jupiter years since you were " + years + " on Earth";
     } else {
       return "Captain, you entered the same stardate";
     }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import Person from './galactic.js';
+// import Person from './galactic.js';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import './css/styles.css';
 
-function handleGalacticForm(event) {
-  event.preventDefault();
-  // do stuff here
-}
+// function handleGalacticForm(event) {
+//   event.preventDefault();
+//   // do stuff here
+// }
 
-window.addEventListener("load", function() {
+// window.addEventListener("load", function() {
   
-});
+// });
