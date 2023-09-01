59722c6a2c9f7ee68987b8ab8f85c19bc4bfc261
Paul LeTourneau <pletourneau62@gmail.com>
2023-09-01 09:50:25 -0700

    refactored tests to only include one person

--- __tests__/javascript.test.js
+++ __tests__/javascript.test.js
@@ -1,23 +1,21 @@
 import Person from './../src/galactic.js';
 
 describe('Character', () => {
-  let paulsPerson;
   let picardPerson;
 
 beforeEach(() => {
-  paulsPerson = new Person("Paul", 41);
   picardPerson = new Person("Captain Jean-Luc Picard", 83);
 });
 
 test('it should return the ageOnEarth and name properties of the Character class instance paulsPerson and picardPerson', () => {
-  expect(paulsPerson).toEqual({name: "Paul", ageOnEarth: 41});
   expect(picardPerson).toEqual({name: "Captain Jean-Luc Picard", ageOnEarth: 83});
 });
 
 test('it should return the ageOnMercury of the Character class instance paulsPerson and picardPerson', () => {
-  expect(paulsPerson.mercuryAge()).toEqual("170.83");
   expect(picardPerson.mercuryAge()).toEqual("345.83");
 });
+
+
 
 
 });
