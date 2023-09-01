da04c4fcc845840bbd806af8837846d0ad30103f
Paul LeTourneau <pletourneau62@gmail.com>
2023-09-01 14:55:55 -0700

    WIP UI

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import './css/styles.css';
 
-// function handleGalacticForm(event) {
-//   event.preventDefault();
-//   // do stuff here
-// }
+function handleGalacticForm(event) {
+  event.preventDefault();
+  const planet = document.querySelector("input[name='planet']:checked").value;
+  console.log(planet);
+  // do stuff here
+}
 
-// window.addEventListener("load", function() {
-  
-// });
+window.addEventListener("load", function() {
+  document.querySelector("form").addEventListener("submit", handleGalacticForm);
+});
