e16bdaaaca48fc4e5166acfdddc426b0fc14e3b5
Paul LeTourneau <pletourneau62@gmail.com>
2023-09-01 08:28:48 -0700

    good fail 1

---
+++ __tests__/javascript.test.js
@@ -0,0 +1,18 @@
+import Person from './../src/galactic.js';
+
+describe('Character', () => {
+  let paulsPerson;
+  let picardPerson;
+
+beforeEach(() => {
+  paulsPerson = new Person("Paul", 41);
+  picardPerson = new Person("Captain Jean-Luc Picard", 83);
+});
+
+test('it should return the properties of the Character class instance paulsPerson and picardPerson', () => {
+  expect(paulsPerson).toEqual({name: "Paul", age: 41});
+  expect(picardPerson).toEqual({name: "Captain Jean-Luc Picard", age: 83});
+});
+
+});
+  
