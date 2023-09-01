2a8c206a264bd57cee4b6ffb194192ce509cc6ba
Paul LeTourneau <pletourneau62@gmail.com>
2023-09-01 13:05:19 -0700

    WIP add UI

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+import Person from './galactic.js';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import './css/styles.css';
+
+function handleGalacticForm(event) {
+  event.preventDefault();
+  // do stuff here
+}
+
+window.addEventListener("load", function() {
+  
+});
